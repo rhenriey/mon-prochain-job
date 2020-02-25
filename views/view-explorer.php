dd11742b61ae867b2498ce57ada3d895481836f1
HENRIEY Raphael <S619121@siege.axa-fr.intraxa>
2020-02-25 09:25:10 +0100

    github.com

---
+++ views/view-explorer.php
@@ -0,0 +1,25 @@
+<div class="">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h4 class="section-title">Explorez la base de données des métiers, des compétences, des savoir-faire... pour voir les métiers et les possibilités de reconversion qui s'offrent à vous !</h4>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="light-grey">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <form class="form">
+          <div class="form-group text-center">
+            <label for="search-db" class="">Indiquez des compétences ou des savoir-faire</label>
+            <input type="text" class="form-control text-center" id="search-db" placeholder="compétence, savoir-faire...">
+          </div>
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div>
